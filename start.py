db26c3efe941a6f75fbb5b43115eef184c23baf9
Helgefy <helge_fylkesnes@hotmail.com>
2017-08-24 18:39:25 +0200

    Fikset albumet

--- start.py
+++ start.py
@@ -1,8 +1,22 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 app.debug = True
+listOfAlbum = os.listdir('static/Album')
+
+
+	
+
+
+def getBilder(albums):
+	ut = {}
+	for album in albums:
+		ut[album] = os.listdir('static/Album/' + album)
+	return ut
+
+bildeListe = getBilder(listOfAlbum)
+
 
 @app.route('/')
 def home():
@@ -11,3 +25,18 @@ def home():
 @app.route('/album')
 def album():
 	return render_template('album.html')
+
+@app.route('/getAlb', methods=['GET'])
+def getAlb():
+	return jsonify(listOfAlbum)
+
+@app.route('/getBilder/<album>', methods=['GET'])
+def getBild(album):
+	if(album in listOfAlbum):
+		return jsonify({'bilder': bildeListe[album], 'album': album})
+	else:
+		return jsonify({'bilder': bildeListe[listOfAlbum[0]], 'album': listOfAlbum[0]})
+
+
+if __name__ == '__main__':
+	main()
--- static/album.js
+++ static/album.js
@@ -12,34 +12,83 @@
    // Listen for the jQuery ready event on the document
    $(function() {
 
-    // Open the modal
-     var $modal = $('.modal1');
-     var $navbar = $('.navbar');
+    //Getting albums
+    var albumer
 
-     var $img = $('.alBilde');
-     var $modalImg = $('#img01');
-     var $caption = $('#caption-tekst');
-     var img;
-     var clickedImg 
+    $.ajax({
+      url: '/getAlb',
+      type: 'GET',
+      method: 'GET',
+      success: function(resp){
+        albumer = resp
+        makeAlbList(albumer);
+        getImgs(window.location.hash.substring(1));// substring(1) to not get # symbol
 
-     // Åpner modal når den blir klikket
-     $img.click(function(){
-      $img.each(function(){ 
-        if ($(this).is(':hover'));{
-          clickedImg = $(this)[0];
+      }
+    });
+    var $albumList = $('#albumList');
+    // Writing albus as li
+    var makeAlbList = function(albumer){
+      var album
+      for (album in albumer){
+        $albumList.append('<li><a href="#'+ albumer[album] + '" class="albumKnapp" >' + albumer[album] + ' </a></li>');
+      }
+    }
+    // Getting images in album
+    var getImgs = function(hashUrl){
+      if (!hashUrl){
+        hashUrl = 'dummy'
+      }
+      $.ajax({
+        url: '/getBilder/' + hashUrl,
+        type: 'GET',
+        method: 'GET',
+        success: function(resp){
+          var bildeListe = resp.bilder;
+          makeImgs(bildeListe,resp.album);
         }
       });
-      $modal.css('display', 'block');
-      $navbar.css('display', 'none');
-      $modalImg.attr('src', clickedImg.src);
-      $caption.text(clickedImg.alt);
-     });
-     // Lukker modal
-     var $span = $('.closer')
-     $span.click(function(){
+
+    }
+    var $bilder = $('.bilder');
+    // Writing images to html
+    var makeImgs = function(imgs, album){
+      var img;
+      for (img in imgs){
+        $bilder.append( '<div class="card col-md-4"> <img class="img-responsive img-thumbnail alBilde" src="static/Album/' + album +'/'+ imgs[img] + '" id="alBilde" alt="' + img + '"><p class="card-text">Her står en beskrivelse til bildet som er over. Teksten kan være ok lang:</p></div>');
+      }
+    }
+    
+    // Album updates on click
+    $albumList.delegate('.albumKnapp','click',function(){
+      $bilder.empty();
+      getImgs($(this).attr('href').substring(1));// substring(1) to not get # symbol
+    });
+
+
+
+
+    // Open the modal
+    var $modal = $('.modal1');
+    var $navbar = $('.navbar');
+    var $modalImg = $('#img01');
+    var $caption = $('#caption-tekst');
+    var clickedImg;
+
+     // Åpner modal når den blir klikket
+    $bilder.delegate('.alBilde','click',function(){
+        clickedImg = $(this)[0];
+        $modal.css('display', 'block');
+        $navbar.css('display', 'none');
+        $modalImg.attr('src', clickedImg.src);
+        $caption.text(clickedImg.alt);
+    });
+    // Lukker modal
+    var $span = $('.closer')
+    $span.click(function(){
       $modal.css('display', 'none')
       $navbar.css('display', 'block');
-     });
+    });
 
     //DOM code
 
